1536098208b0d23d80f9cbe13dfc7a626f195c06
YAHIA ABDCHAFEE Adam <adam.y.abdc@gmail.com>
2024-01-23 15:28:01 +0100

    init project

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="#"
+                  href="https://www.linkedin.com/in/yahia-abechafee/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="#"
+                  href="https://www.linkedin.com/in/yahia-abechafee/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
